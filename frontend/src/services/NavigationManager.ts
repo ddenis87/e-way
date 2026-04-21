3b8dd6cb78a806c6fa52501baab9fcb45bfe4498
Denis Dontsov <denis.dontsov@conjointly.com>
2026-04-21 19:53:24 +0900

    Fixed router

--- frontend/src/services/NavigationManager.ts
+++ frontend/src/services/NavigationManager.ts
@@ -1,13 +1,15 @@
-import {useRouter} from 'vue-router';
 import useAuthManager from './AuthManager';
+import type {Router} from 'vue-router';
 
 const NAV_LOGIN = 'login';
 const NAV_USER_ROLE = 'user-role';
 
 const { state: stateAuth } = useAuthManager();
-const router = useRouter();
+let router: Router|null = null;
 
-function initialize() {
+function initialize(r: Router) {
+    router = r;
+
     router.beforeEach((to, _, next) => {
         if (to.name !== NAV_LOGIN && !stateAuth.isLogin) {
             // Если не авторизован и идет не на страницу логина — на логин
@@ -22,6 +24,7 @@ function initialize() {
 }
 
 function navigationReplace(to: string) {
+    // @ts-expect-error: router может быть null, но мы гарантируем, что initialize будет вызван до этого метода
     router.replace({name: to});
 }
 
